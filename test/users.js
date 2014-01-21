a7dda0b4f1dd914ace7e37e8fc67eead3a610f5b
fariaserick <erick@fanfair.fm>
2014-01-21 15:24:03 -0800

    first commit

---
+++ test/users.js
@@ -0,0 +1,61 @@
+process.env.NODE_ENV = 'test';
+var assert 	= require('assert'),
+	Browser = require('zombie')
+	//app		= require('../../server')
+	;
+
+	describe('Index', function(){
+		
+		/*before(function(){
+			this.server = http.createServer(app).listen(3000);
+		)};*/
+
+		it('should load the form', function(done){
+			var browser = new Browser();
+
+			browser.on("error", function(){
+				console.error(error);
+			});
+
+			browser.visit('http://localhost:9000/', function(){
+				/*then(function(){
+					assert.equal(browser.text("input"), "teste");
+					return browser.clickLink
+				});*/
+
+				browser
+				.fill('#Name', 'Daniel Teixeira')
+				.select('#Born', '1970')
+				.check('#Agree');
+				/*
+				.pressButton('#Submit', function(){
+					assert.equal(browser.location.pathname, '/success');
+					assert.equal(browser.text('#message'),
+						'Thank you for submitting this form!');
+				});
+				*/
+
+				// SHOW ME THE DOCUMENT CONTENTS.
+				//console.log(browser.html());
+
+				// CHECK THE DOCUMENT TITLE.
+				assert.equal(browser.text("title"), "zombieTest");
+
+				// MAKE SURE WE HAVE AN ELEMENT WITH ID NAME
+				assert.ok (browser.query("#Name"));
+
+				assert.ok(browser.success, 'page loaded!');
+				
+				if(browser.error)
+					console.dir("Errors reported:", browser.errors);
+
+				done();
+			});
+		});
+	});
+
+	/*
+	after(function(done){
+		//app.server.close(done);
+	)};
+	*/
