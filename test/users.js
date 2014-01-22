1404cc0159bb1bb377714ae9a04781fcae95e721
fariaserick <erick@fanfair.fm>
2014-01-22 14:59:25 -0800

    commit users.js clean

--- test/users.js
+++ test/users.js
@@ -1,14 +1,9 @@
 process.env.NODE_ENV = 'test';
 var assert 	= require('assert'),
 	Browser = require('zombie')
-	//app		= require('../app')
 	;
 
 	describe('Index', function(){
-		
-		/*before(function(){
-			this.server = http.createServer(app).listen(3000);
-		)};*/
 
 		it('should load the form', function(done){
 			var browser = new Browser();
@@ -17,45 +12,6 @@ var assert 	= require('assert'),
 				console.error(error);
 			});
 
-			browser.visit('https://github.com/fariaserick/zombieTest/', function(){
-				/*then(function(){
-					assert.equal(browser.text("input"), "teste");
-					return browser.clickLink
-				});*/
-
-				browser
-				.fill('#Name', 'Daniel Teixeira')
-				.select('#Born', '1970')
-				.check('#Agree');
-				/*
-				.pressButton('#Submit', function(){
-					assert.equal(browser.location.pathname, '/success');
-					assert.equal(browser.text('#message'),
-						'Thank you for submitting this form!');
-				});
-				*/
-
-				// SHOW ME THE DOCUMENT CONTENTS.
-				//console.log(browser.html());
-
-				// CHECK THE DOCUMENT TITLE.
-				assert.equal(browser.text("title"), "zombieTest");
-
-				// MAKE SURE WE HAVE AN ELEMENT WITH ID NAME
-				assert.ok (browser.query("#Name"));
-
-				assert.ok(browser.success, 'page loaded!');
-				
-				if(browser.error)
-					console.dir("Errors reported:", browser.errors);
-
-				done();
-			});
+			done();
 		});
 	});
-
-	/*
-	after(function(done){
-		//app.server.close(done);
-	)};
-	*/
